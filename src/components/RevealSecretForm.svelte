5bcb20c2d5547d51cc60a85536e2c26c186db565
Slawek Kolodziej <hfrntt@gmail.com>
2023-09-02 14:05:35 +0200

    refactor: clean up heading on reveal secret page

--- src/components/RevealSecretForm.svelte
+++ src/components/RevealSecretForm.svelte
@@ -43,42 +43,43 @@
   function conceal() {
     secret = "";
   }
+
+  const headingSuffix = destruct
+    ? '<br/><span class="text-red-500">or after you read it.</span>'
+    : '.'
+  const heading = `This secret will self-destruct in ${expireIn}${headingSuffix}`;
 </script>
 
-{#if secret}
-  <div class="space-y-8">
-    <TextArea
-      label="Secret:"
-      id="secret"
-      readOnly
-      bind:value={secret}
-      onClick={handleSecretClick}
-    />
+<div class="space-y-4">
+  <h2 class="text-xl">
+    {@html heading}
+  </h2>
 
-    <div class="flex items-center justify-center space-x-2">
-      <Button on:click={conceal}>Conceal</Button>
-      <a
-        href="/"
-        class="text-gray-400 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Create shared secret
-      </a>
+  {#if secret}
+    <div class="space-y-8">
+      <TextArea
+        label="Secret:"
+        id="secret"
+        readOnly
+        bind:value={secret}
+        onClick={handleSecretClick}
+      />
+
+      <div class="flex items-center justify-center space-x-2">
+        <Button on:click={conceal}>Conceal</Button>
+        <a
+          href="/"
+          class="text-gray-400 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Create shared secret
+        </a>
+      </div>
     </div>
-  </div>
-{:else}
-  <div class="space-y-4">
-    <h2 class="text-xl">
-      This secret will self-destruct in {expireIn}{#if destruct}
-        <br/>
-        <span class="text-red-500">or after you read it.</span>
-      {:else}
-        .
-      {/if}
-    </h2>
+  {:else}
     <form on:submit|preventDefault={handleDecrypt} class="space-y-8">
       <Input
         id="passphrase"
-        label="Passphrase:"
+        label="Password:"
         name="hello"
         error={errors.passphrase}
         bind:value={passphrase}
@@ -94,5 +95,5 @@
         <Button type="submit">Reveal secret</Button>
       </div>
     </form>
-  </div>
-{/if}
+  {/if}
+</div>
