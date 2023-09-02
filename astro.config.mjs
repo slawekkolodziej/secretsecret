18df7feb264566678f91aa4ecc4d311917630ea8
Slawek Kolodziej <hfrntt@gmail.com>
2023-09-02 13:08:11 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: vercel()
 });
